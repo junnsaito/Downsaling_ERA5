59ac76b06aa8e04b7c8d3e5345d2144768c8754f
junnsaito <j.saito1102@gmail.com>
2025-02-27 16:23:36 +0900

    Update files with latest changes

--- config.py
+++ config.py
@@ -6,16 +6,22 @@ Created on Thu Feb 27 13:31:58 2025
 @author: junsaito
 """
 
-# config.py
-# グローバル設定
+import os
 
-# 複数のターゲット座標が記載されたCSVファイルのパス
-target_coords_csv = "/media/junsaito/E4308E2C308E05B0/JS_model/target_coords.csv"  # CSV例：列 "lon" と "lat"
+# `config.py` があるディレクトリ（JS_modelの絶対パスを取得）
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-rema_dem_file = "/media/junsaito/E4308E2C308E05B0/Antarctica/REMA/rema_mosaic_500m_v2.0_dem_station_wgs84.tif"
-era5_base_dir = "/media/junsaito/E4308E2C308E05B0/Antarctica/ERA5"
-modis_wgs84_file = "/media/junsaito/E4308E2C308E05B0/Antarctica/MODIS_QA_latest/MODIS_Albedo_2000-2024_avg_wgs84.tif"
-output_dir = "/media/junsaito/E4308E2C308E05B0/JS_model/Output"
+# データパス
+target_coords_csv = os.path.join(BASE_DIR,"csv", "target_coords.csv")
+
+rema_dem_file = os.path.join(BASE_DIR, "Tiff", "rema_mosaic_500m_v2.0_dem_station_wgs84.tif")
+era5_base_dir = os.path.join(BASE_DIR, "ERA5")
+modis_wgs84_file = os.path.join(BASE_DIR, "Tiff", "MODIS_Albedo_2000-2024_avg_wgs84.tif")
+output_dir = os.path.join(BASE_DIR, "Output")
+
+# 必要なら `Output` フォルダを作成
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
 
 z0_station = 0
 kappa_value = 0.35
@@ -27,15 +33,13 @@ monthly_beta = {
     7: 8.23, 8: 8.83, 9: 8.06, 10: 9.83, 11: 9.69, 12: 9.04
 }
 
-# 観測所データのファイルパス
-station_data_file = "/media/junsaito/E4308E2C308E05B0/Antarctica/Showa/AT/Showa_AT_hourly_1980_2024.csv"
-station_data2_file = "/media/junsaito/E4308E2C308E05B0/Antarctica/Showa/DT/Showa_DT_hourly_1980_2024.csv"
+# 観測所データ
+station_data_file = os.path.join(BASE_DIR, "csv", "Showa_AT_hourly_1980_2024.csv")
+station_data2_file = os.path.join(BASE_DIR, "csv", "Showa_DT_hourly_1980_2024.csv")
 station_elev = 29.0
 
-
-# 氷流速度（ベクトル）データのファイルパス
-vx_tif_path = '/media/junsaito/E4308E2C308E05B0/Antarctica/ice_flow/measure_velocity_450m_vx_shirase_lang_new.tif'
-vy_tif_path = '/media/junsaito/E4308E2C308E05B0/Antarctica/ice_flow/measure_velocity_450m_vy_shirase_lang_new.tif'
-vx_error_tif_path = '/media/junsaito/E4308E2C308E05B0/Antarctica/ice_flow/measure_velocity_450m_errvx_shirase_lang_new.tif'
-vy_error_tif_path = '/media/junsaito/E4308E2C308E05B0/Antarctica/ice_flow/measure_velocity_450m_errvy_shirase_lang_new.tif'
-shapefile_path = '/media/junsaito/E4308E2C308E05B0/Antarctica/iceshelved_wgs84.shp'
+# 氷流速度（ベクトル）データ
+vx_tif_path = os.path.join(BASE_DIR, "Tiff", "measure_velocity_450m_vx_shirase_lang_new.tif")
+vy_tif_path = os.path.join(BASE_DIR, "Tiff", "measure_velocity_450m_vy_shirase_lang_new.tif")
+vx_error_tif_path = os.path.join(BASE_DIR, "Tiff", "measure_velocity_450m_errvx_shirase_lang_new.tif")
+vy_error_tif_path = os.path.join(BASE_DIR, "Tiff", "measure_velocity_450m_errvy_shirase_lang_new.tif")
